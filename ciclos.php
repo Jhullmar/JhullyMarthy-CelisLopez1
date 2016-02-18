062e56b8f5a92a80e424e796e945b1ae2704d161
jully <jhullyceliz@gmail.com>
2016-02-18 18:52:02 -0500

    mi segundo commit

---
+++ ciclos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Ciclos<title>
+	</head>
+</body>
+	<?php for ($i=0; $i<9; $i++;) { ?>
+	<h1> CICLOS</h1>
+	<?php };?>
+
+	<?php for ($i=0; $i<9; $i++;) { 
+	echo "<h1> CICLOS</h1>";
+	} ?>
+
+
+
+</body>
+</html>
